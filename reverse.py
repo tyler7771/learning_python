d55cb4925eb246de0f8d3fc15cad3054e19a52fc
Tyler Fields <tyler.fields777@gmail.com>
2017-01-25 16:06:39 -0800

    initial commit

---
+++ reverse.py
@@ -0,0 +1,24 @@
+# Write a method that will take a string as input, and return a new
+# string with the same letters in reverse order.
+#
+# Don't use String's reverse method; that would be too simple.
+#
+# Difficulty: easy.
+
+def reverse(string):
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+print("\nTests for #reverse")
+print("===============================================")
+    print(
+      'reverse("abc") == "cba": ' + (reverse("abc") == "cba").to_s
+    )
+    print(
+      'reverse("a") == "a": ' + (reverse("a") == "a").to_s
+    )
+    print(
+      'reverse("") == "": ' + (reverse("") == "").to_s
+    )
+print("===============================================")
